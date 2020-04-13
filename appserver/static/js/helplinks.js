e46f4b134e0907970499895025963dbec53238f9
ani0989 <11317519+ani0989@users.noreply.github.com>
2020-04-12 23:51:54 -0700

    helplinks updated

--- appserver/static/js/helplinks.js
+++ appserver/static/js/helplinks.js
@@ -9,13 +9,13 @@ document.addEventListener("DOMNodeInserted", function (event) {
     }
 }, false);
 
-function setValue(val1) {
-    console.log(val1);
-    // var element = document.getElementById('additional_parameters-access_token');
-    // element.value = val1;
-    // element.innerHTML = val1;
-    $('#additional_parameters-access_token').val(val1);
-}
+// function setValue(val1) {
+//     console.log(val1);
+//     // var element = document.getElementById('additional_parameters-access_token');
+//     // element.value = val1;
+//     // element.innerHTML = val1;
+//     $('#additional_parameters-access_token').val(val1);
+// }
 
 function clickEvent(url) {
     var redirect_uri = window.location.href.split('app')[0] + 'static/app/TA-splunk-add-on-for-slack/html/index.html';
