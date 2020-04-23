a0c9ea196ab781982cc20525d12d0ece33eae939
ani0989 <11317519+ani0989@users.noreply.github.com>
2020-04-23 15:27:19 -0700

    Changed constants and logging

--- bin/input_module_slack_audit_logs.py
+++ bin/input_module_slack_audit_logs.py
@@ -21,25 +21,27 @@ def validate_input(helper, definition):
         helper.log_info("Start time valid")
     except ValueError:
         raise ValueError("Incorrect data format, should be YYYY-MM-DD HH:MM:SS")
-    
-    access_token = definition.parameters.get('access_token', None)
-    validate_access_token(helper, access_token)
-    
 
-def validate_access_token(helper, access_token):
-    validate_access_token_url = constants.SLACK_ACCESS_TOKEN_CHECK_URL + access_token
+def validate_access_token(helper, access_token, INPUT_STANZA_NAME):
+    validate_access_token_url = constants.SLACK_ACCESS_TOKEN_CHECK_URL
     try:
-        response = json.loads(requests.get(validate_access_token_url).text)
+        helper.log_info("{} - Validation request sent to URL : {}".format(INPUT_STANZA_NAME, validate_access_token_url))
+        header = {
+            'Content-type': 'application/json', 
+            'Authorization': 'Bearer ' + access_token
+        }
+        response = json.loads(requests.get(validate_access_token_url, headers=header, proxies=get_proxy(helper)).text)
+        helper.log_info("{} - Response : {}".format(INPUT_STANZA_NAME, response))
         if response["ok"]:
-            helper.log_info("Access Token Valid")
+            helper.log_info("{} - Access Token Valid".format(INPUT_STANZA_NAME))
         else:
             raise requests.exceptions.RequestException
     except Exception as e:
         if response["ok"]:
-            helper.log_error("Response received : {}".format(response["ok"]))
+            helper.log_error("{} - Response received : {}".format(INPUT_STANZA_NAME, response["ok"]))
         if response["error"]:
-            helper.log_error("Response error : {}".format(response["error"]))
-        helper.log_error(requests.exceptions.RequestException("Request Exception")) 
+            helper.log_error("{} - Response error : {}".format(INPUT_STANZA_NAME, response["error"]))
+        helper.log_error(requests.exceptions.RequestException("{} - Request Exception".format(INPUT_STANZA_NAME))) 
     
 def get_proxy(helper):
     proxy = helper.get_proxy()
@@ -59,14 +61,12 @@ def get_proxy(helper):
         }
     return proxies
 
-
 def convert_utc_to_epoch(timestamp_string):
     '''Use this function to convert utc to epoch'''
     timestamp = datetime.datetime.strptime(
         timestamp_string, '%Y-%m-%d %H:%M:%S')
     epoch = int(calendar.timegm(timestamp.utctimetuple()))
     return epoch
-
 
 def get_start_date(helper, check_point_key):
     # Try to get a date from the check point first
@@ -81,22 +81,22 @@ def get_start_date(helper, check_point_key):
         if (d not in [None, '']):
             return convert_utc_to_epoch(d)
 
-def make_request(helper, base_url, epoch_time, opt_access_token):
+def make_request(helper, base_url, epoch_time, opt_access_token, INPUT_STANZA_NAME):
     header = {'User-Agent': 'Splunk-AuditApiClient/1.0.0',
               'Accept': 'application/json',
               'Authorization': 'Bearer ' + opt_access_token
               }
-    helper.log_info("Request sent to url : {}, header : {}, proxy info : {}".format(base_url, header, get_proxy(helper)))
+    helper.log_info("{} - Request sent to url : {}, header : {}, proxy info : {}".format(INPUT_STANZA_NAME, base_url, header, get_proxy(helper)))
     r = requests.get(base_url, headers=header, proxies=get_proxy(helper))
     return r
 
-def handle_audit_logs(helper, auditlogs, epoch_time, max_auditDateTime, check_point_key, ew):
+def handle_audit_logs(helper, auditlogs, epoch_time, max_auditDateTime, check_point_key, ew, INPUT_STANZA_NAME):
     if 'entries' in auditlogs:
-        helper.log_info("Count of auditlogs received : {}".format(len(auditlogs["entries"])))
+        helper.log_info("{} - Count of auditlogs received : {}".format(INPUT_STANZA_NAME, len(auditlogs["entries"])))
         for entry in auditlogs["entries"]:
             this_auditDateTime = entry.get('date_create')
             if(this_auditDateTime <= epoch_time):
-                helper.log_info("Events returned during this call were already ingested. So skipping ...")
+                helper.log_info("{} - Events returned during this call were already ingested. So skipping ...".format(INPUT_STANZA_NAME))
                 # helper.log_info("Skipped event : {}".format(entry))
                 continue
             if(this_auditDateTime > max_auditDateTime):
@@ -106,46 +106,53 @@ def handle_audit_logs(helper, auditlogs, epoch_time, max_auditDateTime, check_po
             ew.write_event(e)
         return max_auditDateTime
 
+def get_logs(helper, ew, epoch_time, opt_access_token, INPUT_STANZA_NAME, check_point_key):
+    # Make the first request for the Audit logs
+    base_url = constants.SLACK_AUDIT_LOG_URL + "{}".format(epoch_time) + "&limit=" + constants.SLACK_AUDIT_LOG_LIMIT
+    r = make_request(helper, base_url, epoch_time, opt_access_token, INPUT_STANZA_NAME)
+    
+    try:
+        r.raise_for_status()
+        auditlogs = json.loads(r.text)
+        max_auditDateTime = epoch_time
+        # Ingest Audit logs
+        max_auditDateTime = handle_audit_logs(helper, auditlogs, epoch_time, max_auditDateTime, check_point_key, ew, INPUT_STANZA_NAME)
+        # Handling Pagination
+        next_cursor = auditlogs['response_metadata']['next_cursor']
+        while next_cursor:
+            base_url = base_url + "&cursor=" + next_cursor
+            r = make_request(helper, base_url, epoch_time, opt_access_token, INPUT_STANZA_NAME)
+            try:
+                r.raise_for_status()
+                auditlogs = json.loads(r.text)
+                next_cursor = auditlogs['response_metadata']['next_cursor']
+                max_auditDateTime = handle_audit_logs(helper, auditlogs, epoch_time, max_auditDateTime, check_point_key, ew, INPUT_STANZA_NAME)
+            except Exception as e:
+                message = "{} - HTTP Request error: {}".format(INPUT_STANZA_NAME, e)
+                helper.log_error(message)
+                sys.exit(message)
+        helper.save_check_point(check_point_key, max_auditDateTime)
+    except Exception as e:
+        message = "{} - HTTP Request error: {}".format(INPUT_STANZA_NAME, e)
+        helper.log_error(message)
+        sys.exit(message)
+
 def collect_events(helper, ew):
-    helper.log_info("Slack Audit addon start execution")
+    INPUT_STANZA_NAME = helper.get_input_stanza_names()
+    helper.log_info("{} - Slack Audit addon start execution".format(INPUT_STANZA_NAME))
+    
     # Retrieve Input paramters
-    opt_access_token = helper.get_arg('access_token')
     opt_start_time = helper.get_arg('start_time')
+    opt_access_token = helper.get_arg('access_token')
+    # Validate Access Token
+    validate_access_token(helper, opt_access_token, INPUT_STANZA_NAME)
     
     # Retrieve the name of the input's checkpoint
     check_point_key = "{}_last_date".format(helper.get_input_stanza_names())
     # Retrieve the latest checkpoint
     epoch_time = get_start_date(helper, check_point_key)
     
-    # Make the first request for the Audit logs
-    base_url = constants.SLACK_AUDIT_LOG_URL + "{}".format(epoch_time) + "&limit=" + constants.SLACK_AUDIT_LOG_LIMIT
-    r = make_request(helper, base_url, epoch_time, opt_access_token)
-
-    try:
-        r.raise_for_status()
-        auditlogs = json.loads(r.text)
-        max_auditDateTime = epoch_time
-        # Ingest Audit logs
-        max_auditDateTime = handle_audit_logs(helper, auditlogs, epoch_time, max_auditDateTime, check_point_key, ew)
-        # Handling Pagination
-        next_cursor = auditlogs['response_metadata']['next_cursor']
-        while next_cursor:
-            base_url = base_url + "&cursor=" + next_cursor
-            r = make_request(helper, base_url, epoch_time, opt_access_token)
-            try:
-                r.raise_for_status()
-                auditlogs = json.loads(r.text)
-                next_cursor = auditlogs['response_metadata']['next_cursor']
-                max_auditDateTime = handle_audit_logs(helper, auditlogs, epoch_time, max_auditDateTime, check_point_key, ew)
-            except Exception as e:
-                message = "HTTP Request error: %s" % str(e)
-                helper.log_error(message)
-                sys.exit(message)
-        helper.save_check_point(check_point_key, max_auditDateTime)
-    except Exception as e:
-        message = "HTTP Request error: %s" % str(e)
-        helper.log_error(message)
-        sys.exit(message)
-
-    helper.log_info("Slack Audit addon completed execution")
+    get_logs(helper, ew, epoch_time, opt_access_token, INPUT_STANZA_NAME, check_point_key)
+    
+    helper.log_info("{} - Slack Audit addon completed execution".format(INPUT_STANZA_NAME))
     
--- bin/slack_audit_logs_constants.py
+++ bin/slack_audit_logs_constants.py
@@ -1,3 +1,3 @@
-SLACK_ACCESS_TOKEN_CHECK_URL = "https://slack.com/api/auth.test?pretty=1&access_token="
+SLACK_ACCESS_TOKEN_CHECK_URL = "https://slack.com/api/auth.test?pretty=1"
 SLACK_AUDIT_LOG_URL = "https://api.slack.com/audit/v1/logs?oldest="
 SLACK_AUDIT_LOG_LIMIT = "200"
